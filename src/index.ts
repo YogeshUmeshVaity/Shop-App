f57e662c2fe8babdcc212c95b5cde5c773b65915
Yogesh Umesh Vaity <yogeshumeshvaity@gmail.com>
2020-10-13 01:14:17 +0530

    Add a simple Express server.

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,30 @@
-import express, { Request, Response } from 'express'
-
+import express from 'express'
 const app = express()
 
-app.get('/', (request: Request, response: Response) => {
-  response.send(`Welcome to Express!`)
+app.get(
+  '/user/:id',
+  function (req, res, next) {
+    console.log('before request handler function 1')
+    next('route')
+  },
+  function (req, res, next) {
+    // Won't reach here.
+    console.log('before request handler function 2')
+    next()
+  }
+)
+
+app.get('/user/:id', function (req, res, next) {
+  console.log('handling request')
+  res.sendStatus(200)
+  // This won't make any difference, since there is just one function.
+  // This should be next()
+  next('route')
+})
+
+app.get('/user/:id', function (req, res, next) {
+  console.log('after request handler')
+  next()
 })
 
 app.listen(3000)

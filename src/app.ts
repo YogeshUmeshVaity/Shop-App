51eac97be080cb3c784082e8ff1ee5a51a2292f3
Yogesh Umesh Vaity <yogeshumeshvaity@gmail.com>
2020-10-14 01:17:46 +0530

    Add body-parser to parse data from the form.

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,9 @@
 import express, { Request, Response } from 'express'
+import bodyParser from 'body-parser'
+
 const app = express()
+
+app.use(bodyParser.urlencoded({ extended: false }))
 
 app.get('/', function (request: Request, response: Response) {
   response.send(`<h1>Hello from Express!</h1>`)

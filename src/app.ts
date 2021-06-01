68186df9e104bbaefe1e31171294c67378d3cd43
Yogesh Umesh Vaity <yogeshumeshvaity@gmail.com>
2021-06-01 18:43:53 +0530

    Add the HttpException class for representing the middleware error in a type safe manner.

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,8 @@
-import express, { Request, Response } from 'express'
+import express, { NextFunction, Request, Response } from 'express'
 import bodyParser from 'body-parser'
 import { adminRoutes } from './routes/admin'
 import { shopRoutes } from './routes/shop'
+import { HttpException } from './exceptions/HttpException'
 
 const app = express()
 
@@ -9,5 +10,15 @@ app.use(bodyParser.urlencoded({ extended: false }))
 
 app.use('/admin', adminRoutes)
 app.use('/', shopRoutes)
+
+app.use((request: Request, response: Response) => {
+  response.status(404).send(`<h1>Page not found!</h1>`)
+})
+
+app.use((err: HttpException, req: Request, res: Response, next: NextFunction) => {
+  err.status = 404
+  err.message = 'Not Found'
+  next(err)
+})
 
 app.listen(3000)
--- src/routes/shop.ts
+++ src/routes/shop.ts
@@ -2,6 +2,6 @@ import express, { Request, Response } from 'express'
 
 export const shopRoutes = express.Router()
 
-shopRoutes.get('/', function (request: Request, response: Response) {
+shopRoutes.get('/', (request: Request, response: Response) => {
   response.send(`<h1>Hello from Express!</h1>`)
 })

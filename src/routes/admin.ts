b1ee00f81a6d8866253974b0d7b3e8ff97c4a9b7
Yogesh Umesh Vaity <yogeshumeshvaity@gmail.com>
2021-06-02 20:28:07 +0530

    Update the admin routes and shop routes to send the respective HTML pages for add-product and list of items.

--- src/routes/admin.ts
+++ src/routes/admin.ts
@@ -1,11 +1,10 @@
 import express, { Request, Response } from 'express'
+import path from 'path'
 
 export const adminRoutes = express.Router()
 
 adminRoutes.get('/add-product', (request: Request, response: Response) => {
-  response.send(
-    '<form action="/admin/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>'
-  )
+  response.sendFile(path.join(__dirname, '..', 'views', 'add-product.html'))
 })
 
 adminRoutes.post('/product', (request: Request, response: Response) => {
--- src/routes/shop.ts
+++ src/routes/shop.ts
@@ -1,7 +1,10 @@
 import express, { Request, Response } from 'express'
+import path from 'path'
 
 export const shopRoutes = express.Router()
 
+// path.join() takes care of separators in a platform independent manner.
+// __dirname points to the location of this file. So, we go one directory up by spefying double dot.
 shopRoutes.get('/', (request: Request, response: Response) => {
-  response.send(`<h1>Hello from Express!</h1>`)
+  response.sendFile(path.join(__dirname, '..', 'views', 'shop.html'))
 })

3d6ad7950c81b01a29dba5b8903ffe186587ca18
Yogesh Umesh Vaity <yogeshumeshvaity@gmail.com>
2021-06-04 19:57:17 +0530

    Create an array for the storing the products submitted by the user and display it in console.

--- src/routes/admin.ts
+++ src/routes/admin.ts
@@ -1,8 +1,10 @@
 import express, { Request, Response } from 'express'
 import path from 'path'
 import { rootDirectory } from '../util/path'
+import { Product } from '../models/product'
 
 export const adminRoutes = express.Router()
+export const products: Array<Product> = []
 
 adminRoutes.get('/add-product', (request: Request, response: Response) => {
     response.sendFile(path.join(rootDirectory(), 'views', 'add-product.html'))
@@ -10,5 +12,6 @@ adminRoutes.get('/add-product', (request: Request, response: Response) => {
 
 adminRoutes.post('/product', (request: Request, response: Response) => {
     console.log(request.body)
+    products.push({ title: request.body.title })
     response.redirect('/')
 })
--- src/routes/shop.ts
+++ src/routes/shop.ts
@@ -1,11 +1,13 @@
 import express, { Request, Response } from 'express'
 import path from 'path'
 import { rootDirectory } from '../util/path'
+import { products } from '../routes/admin'
 
 export const shopRoutes = express.Router()
 
 // path.join() takes care of separators in a platform independent manner.
 // __dirname points to the location of this file. So, we go one directory up by specifying double dot.
 shopRoutes.get('/', (request: Request, response: Response) => {
+    console.log(products)
     response.sendFile(path.join(rootDirectory(), 'views', 'shop.html'))
 })

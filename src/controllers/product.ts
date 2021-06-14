0b97aec473f91ae60d3434731f5bc7b25787e38c
Yogesh Umesh Vaity <yogeshumeshvaity@gmail.com>
2021-06-14 21:23:52 +0530

    Save and fetch products to and from a file.

--- src/controllers/product.ts
+++ src/controllers/product.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from 'express'
-import { Product, products } from '../models/product'
+import { Product } from '../models/product'
 
 export const getAddProduct = (request: Request, response: Response): void => {
     return response.render('add-product', {
@@ -18,8 +18,8 @@ export const postAddProduct = (request: Request, response: Response): void => {
     response.redirect('/')
 }
 
-export const getProducts = (request: Request, response: Response): void => {
-    const products = Product.fetchAll()
+export const getProducts = async (request: Request, response: Response): Promise<void> => {
+    const products = await Product.fetchAll()
     response.render('shop', {
         productList: products,
         pageTitle: 'Shop',
--- src/models/product.ts
+++ src/models/product.ts
@@ -1,4 +1,19 @@
+import path from 'path'
+import { rootDirectory } from '../util/path'
+import { promises as fs } from 'fs' // functions in fs.promises return promises
+
 export const products: Array<Product> = []
+
+const filePath = path.join(rootDirectory(), 'data', 'products.json')
+
+async function getProductsFromFile(): Promise<Array<Product>> {
+    try {
+        const fileContents = await fs.readFile(filePath)
+        return JSON.parse(fileContents.toString())
+    } catch {
+        return new Array<Product>()
+    }
+}
 
 export class Product {
     title: string
@@ -7,12 +22,20 @@ export class Product {
         this.title = title
     }
 
-    save(): void {
-        products.push(this)
+    /**
+     * Appends this product to the current products in the file.
+     */
+    async save(): Promise<void> {
+        const currentProducts = await getProductsFromFile()
+        currentProducts.push(this)
+        try {
+            fs.writeFile(filePath, JSON.stringify(currentProducts))
+        } catch (err) {
+            console.log(err)
+        }
     }
 
-    static fetchAll(): Array<Product> {
-        return products
+    static async fetchAll(): Promise<Array<Product>> {
+        return await getProductsFromFile()
     }
 }
-

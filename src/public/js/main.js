c19fae4e7c2df9a78dd80d78df68fd4a05c142ca
Yogesh Umesh Vaity <yogeshumeshvaity@gmail.com>
2021-06-18 17:42:28 +0530

    Document the main.js file in public/js directory.

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -1,7 +1,15 @@
+/**
+ * This script will be executed from inside the <script> tag of end.ejs file.
+ */
+
 const backdrop = document.querySelector('.backdrop')
 const sideDrawer = document.querySelector('.mobile-nav')
 const menuToggle = document.querySelector('#side-menu-toggle')
 
+/**
+ * Backdrop means the screen behind the side-drawer. When the user clicks on the backdrop, the
+ * drawer should disappear.
+ */
 function backdropClickHandler() {
     backdrop.style.display = 'none'
     sideDrawer.classList.remove('open')

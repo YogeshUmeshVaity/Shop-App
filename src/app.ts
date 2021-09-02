3d6e3eec71181cae1ff1dd4e755db16701d0c662
Yogesh Umesh Vaity <yogeshumeshvaity@gmail.com>
2021-09-02 17:51:07 +0530

    Add css for the login form.

--- src/app.ts
+++ src/app.ts
@@ -34,7 +34,7 @@ app.use('/admin', adminRoutes)
 app.use('/', shopRoutes)
 app.use('/', authRoutes)
 
-//app.use(errorController.get404)
+app.use(errorController.get404)
 
 mongoose
     .connect(databaseUrl(), connectOptions)

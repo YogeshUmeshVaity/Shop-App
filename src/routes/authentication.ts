d4adff500411151503d950c8fb2a9a8dcd944c2a
Yogesh Umesh Vaity <yogeshumeshvaity@gmail.com>
2021-10-25 21:28:21 +0530

    Check whether the Password and Confirm Password match while signing up.

--- src/routes/authentication.ts
+++ src/routes/authentication.ts
@@ -15,12 +15,19 @@ authRoutes.get('/signup', authController.getSignup)
 authRoutes.post(
     '/signup',
     check('email').isEmail().withMessage('Please enter a valid email'),
-    check(
+    body(
         'password',
         'Password should contain numbers, text and should be at least 5 characters long.'
     )
         .isLength({ min: 5 })
         .isAlphanumeric(),
+    // We can't rename req as request because it's a property of type Meta.
+    body('confirmPassword').custom((value: string, { req }) => {
+        if (value != req.body.password) {
+            throw new Error('Passwords must match!')
+        }
+        return true
+    }),
     authController.postSignup
 )
 

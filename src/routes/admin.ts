2a1256c4940a77426353ec7a4ddd49b663408f56
Yogesh Umesh Vaity <yogeshumeshvaity@gmail.com>
2021-10-29 20:20:39 +0530

    Add validation error message for add-product and edit-product pages.

--- src/routes/admin.ts
+++ src/routes/admin.ts
@@ -6,6 +6,8 @@ import * as adminController from '../controllers/admin'
 import { authenticate } from '../middleware/authenticate'
 
 export const adminRoutes = express.Router()
+const MIN_CHARS = 8
+const MAX_CHARS = 400
 
 adminRoutes.get('/products', authenticate, adminController.getProducts)
 
@@ -14,10 +16,18 @@ adminRoutes.get('/add-product', authenticate, adminController.getAddProduct)
 adminRoutes.post(
     '/add-product',
     [
-        body('title').isString().isLength({ min: 3 }).trim(),
-        body('imageUrl').isURL(),
-        body('price').isFloat(),
-        body('description').isLength({ min: 8, max: 400 }).trim()
+        body('title', 'Title needs to be a text and minimum 3 characters long.')
+            .isString()
+            .isLength({ min: 3 })
+            .trim(),
+        body('imageUrl', 'Please enter a valid URL.').isURL(),
+        body('price', 'Price needs to be a number.').isFloat(),
+        body(
+            'description',
+            `Description needs to be minimum ${MIN_CHARS} and maximum ${MAX_CHARS}  characters long`
+        )
+            .isLength({ min: MIN_CHARS, max: MAX_CHARS })
+            .trim()
     ],
     authenticate,
     adminController.postAddProduct
@@ -28,10 +38,18 @@ adminRoutes.get('/edit-product/:productId', authenticate, adminController.getEdi
 adminRoutes.post(
     '/edit-product',
     [
-        body('title').isString().isLength({ min: 3 }).trim(),
-        body('imageUrl').isURL(),
-        body('price').isFloat(),
-        body('description').isLength({ min: 8, max: 400 }).trim()
+        body('title', 'Title needs to be a text and minimum 3 characters long.')
+            .isString()
+            .isLength({ min: 3 })
+            .trim(),
+        body('imageUrl', 'Please enter a valid URL.').isURL(),
+        body('price', 'Price needs to be a number.').isFloat(),
+        body(
+            'description',
+            `Description needs to be minimum ${MIN_CHARS} and maximum ${MAX_CHARS} characters long`
+        )
+            .isLength({ min: MIN_CHARS, max: MAX_CHARS })
+            .trim()
     ],
     authenticate,
     adminController.postEditProduct

de8ca459e78fdfb1fbe873e2f080ea3086a9246a
Yogesh Umesh Vaity <yogeshumeshvaity@gmail.com>
2021-10-31 02:52:56 +0530

    Update documentation comment for DatabaseException class.

--- src/exceptions/DatabaseException.ts
+++ src/exceptions/DatabaseException.ts
@@ -3,9 +3,12 @@ import { HttpException } from './HttpException'
 
 /**
  * This exception is thrown whenever an unexpected error occurs while interacting with the database.
+ * Includes the HTTP status code: 500.
  */
 export class DatabaseException extends HttpException {
     constructor(message: string) {
+        // We have mentioned the status code (500) in the super for avoiding the clutter at call site.
+        // This way we only have to mention the message and not the status code.
         super(StatusCodes.INTERNAL_SERVER_ERROR, message)
     }
 }

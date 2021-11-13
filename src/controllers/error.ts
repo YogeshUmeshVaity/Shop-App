3dd02176de2489702d3e7dfb063021b03274519b
Yogesh Umesh Vaity <yogeshumeshvaity@gmail.com>
2021-11-13 19:23:59 +0530

    Rename ReadFileException to FileReadException for naming consistency with FileDeleteException. Move FileReadException to FileException directory.

--- src/controllers/error.ts
+++ src/controllers/error.ts
@@ -2,7 +2,7 @@ import { NextFunction, Request, Response } from 'express'
 import { FileDeleteException } from '../exceptions/FileExceptions/FileDeleteException'
 import { DatabaseException } from '../exceptions/HttpExceptions/DatabaseException'
 import { PaymentException } from '../exceptions/PaymentExceptions/PaymentException'
-import { FileReadException } from '../exceptions/ReadFileException'
+import { FileReadException } from '../exceptions/FileExceptions/FileReadException'
 import { Logger } from '../lib/logger'
 
 export const get404 = (request: Request, response: Response): void => {

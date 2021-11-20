0e0822eb149c479271d581177d0e002272f47f54
Yogesh Umesh Vaity <yogeshumeshvaity@gmail.com>
2021-11-21 01:44:51 +0530

    Convert the database connection code to try/catch instead of then().catch().

--- src/lib/database.ts
+++ src/lib/database.ts
@@ -1,7 +1,6 @@
 import mongoose from 'mongoose'
-import session, { SessionOptions } from 'express-session'
-import mongoDbSession from 'connect-mongodb-session'
 import { Express } from 'express-serve-static-core'
+import https from 'https'
 
 export function databaseUrl(): string {
     const databaseUrl = process.env.DATABASE_URL
@@ -9,13 +8,11 @@ export function databaseUrl(): string {
     return databaseUrl
 }
 
-export const connectOptions = {
+const connectOptions = {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useFindAndModify: false
 }
-
-const MongoDbStore = mongoDbSession(session)
 
 export function sessionSecret(): string {
     const sessionSecret = process.env.SESSION_SECRET
@@ -26,24 +23,11 @@ export function sessionSecret(): string {
 // Generally hosting provider configures the SSL/TLS encryption. When we need to manually configure
 // the SSL, uncomment the https line and remove the 'app' from app.listen.
 // Also don't forget import sslPrivateKey and sslCertificate from util/ssl.ts
-export function connectToDatabase(app: Express): void {
-    mongoose
-        .connect(databaseUrl(), connectOptions)
-        .then(() => {
-            // https.createServer({ key: sslPrivateKey, cert: sslCertificate }, app)
-            app.listen(process.env.PORT || 3000)
-        })
-        .catch((error) => console.log(error))
+export async function connectToDatabase(app: Express): Promise<void> {
+    try {
+        await mongoose.connect(databaseUrl(), connectOptions)
+        app.listen(process.env.PORT || 3000)
+    } catch (error) {
+        console.log(error)
+    }
 }
-
-// export const sessionStore = new MongoDbStore({
-//     uri: databaseUrl(),
-//     collection: 'sessions'
-// })
-
-// export const sessionOptions: SessionOptions = {
-//     secret: sessionSecret(),
-//     resave: false,
-//     saveUninitialized: false,
-//     store: sessionStore
-// }
